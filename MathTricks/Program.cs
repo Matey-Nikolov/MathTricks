9ef0dc76b30c09a8a25695b10771c8699459452b
Martin Pashov <80414148+Martin-Pashov@users.noreply.github.com>
2023-06-03 11:17:48 +0300

    Update Program.cs

--- MathTricks/Program.cs
+++ MathTricks/Program.cs
@@ -2,116 +2,122 @@
 {
     public class Program
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
             /*
             private static GameBoard board;
             private static uint n;
             private static uint m;
             */
 
-            static void Main()
+        static void Main(string[] args)
+        {
+
+            /*
+
+            bool resultN = uint.TryParse(Console.ReadLine(), out n);
+            bool resultM = uint.TryParse(Console.ReadLine(), out m);
+            //Validation(resultN, resultM);
+
+            try
             {
-                /*
 
-                bool resultN = uint.TryParse(Console.ReadLine(), out n);
-                bool resultM = uint.TryParse(Console.ReadLine(), out m);
-                //Validation(resultN, resultM);
+                board = new GameBoard(n, m);
+            }
+            catch(ArgumentOutOfRangeException exp)
+            { 
+                Console.WriteLine(exp.Message);
+            }
+        }*/
 
-                try
+            /*
+            private static void Validation(bool resultN, bool resultM)
+            {
+                if (!resultN && !resultM)
                 {
-
-                    board = new GameBoard(n, m);
-                }
-                catch(ArgumentOutOfRangeException exp)
-                { 
-                    Console.WriteLine(exp.Message);
+                    Console.WriteLine("The inputs is not a valid positive integers.");
                 }
             }*/
 
-                /*
-                private static void Validation(bool resultN, bool resultM)
-                {
-                    if (!resultN && !resultM)
-                    {
-                        Console.WriteLine("The inputs is not a valid positive integers.");
-                    }
-                }*/
+            int width;
+            int height;
 
-                int height, width;
-
-                do
-                {
-                    Console.Write("Enter the height of the board (minimum 4): ");
-                } while (!int.TryParse(Console.ReadLine(), out height) || height < 4);
-
-                do
-                {
-                    Console.Write("Enter the width of the board (minimum 4): ");
-                } while (!int.TryParse(Console.ReadLine(), out width) || width < 4);
-
-                Console.Clear();
-                Console.WriteLine("Matrix Board Game");
-                Console.WriteLine("-----------------");
-
-                // Create the board matrix
-                char[,] board = new char[height, width];
-
-                // Fill the board with empty spaces
-                for (int row = 0; row < height; row++)
-                {
-                    for (int col = 0; col < width; col++)
-                    {
-                        board[row, col] = ' ';
-                    }
-                }
-
-                // Display the board
-                DisplayBoard(board);
-            }
-
-            static void DisplayBoard(char[,] board)
+            do
             {
-                int height = board.GetLength(0);
-                int width = board.GetLength(1);
+                Console.Write("Enter the height of the board (minimum 4): ");
+            } 
+            
+            while (!int.TryParse(Console.ReadLine(), out height) || height < 4);
 
-                // Display column headers
-                Console.Write("   ");
+            do
+            {
+                Console.Write("Enter the width of the board (minimum 4): ");
+            } 
+            
+            while (!int.TryParse(Console.ReadLine(), out width) || width < 4);
+
+            Console.Clear();
+            Console.WriteLine("Matrix Board Game");
+            Console.WriteLine("-----------------");
+
+            // Create the board matrix
+            char[,] board = new char[height, width];
+
+            // Fill the board with empty spaces
+            for (int row = 0; row < height; row++)
+            {
                 for (int col = 0; col < width; col++)
                 {
-                    Console.Write($" {col + 1,3}");
+                    board[row, col] = ' ';
                 }
+            }
+
+            // Display the board
+            DisplayBoard(board);
+        }
+
+
+        static void DisplayBoard(char[,] board)
+        {
+            int height = board.GetLength(0);
+            int width = board.GetLength(1);
+
+            // Display column headers
+            Console.Write("    ");
+            for (int col = 0; col < width; col++)
+            {
+                if (col == 0)
+                    Console.Write($"{col + 1,3}");
+                else
+                    Console.Write($"{col + 1,6}");
+            }
+            Console.WriteLine();
+
+            // Display horizontal line
+            Console.Write("   ");
+            for (int col = 0; col < width; col++)
+            {
+                Console.Write("------");
+            }
+            Console.WriteLine();
+
+            // Display rows with board content
+            for (int row = 0; row < height; row++)
+            {
+                Console.Write($"{row + 1,2} |");
+
+                for (int col = 0; col < width; col++)
+                {
+                    Console.Write($" {board[row, col],-3} |");
+                }
+
                 Console.WriteLine();
 
                 // Display horizontal line
                 Console.Write("   ");
                 for (int col = 0; col < width; col++)
                 {
-                    Console.Write("----");
+                    Console.Write("------");
                 }
                 Console.WriteLine();
-
-                // Display rows with board content
-                for (int row = 0; row < height; row++)
-                {
-                    Console.Write($"{row + 1,2} |");
-
-                    for (int col = 0; col < width; col++)
-                    {
-                        Console.Write($" {board[row, col],-3} |");
-                    }
-
-                    Console.WriteLine();
-
-                    // Display horizontal line
-                    Console.Write("   ");
-                    for (int col = 0; col < width; col++)
-                    {
-                        Console.Write("----");
-                    }
-                    Console.WriteLine();
-                }
             }
         }
     }
