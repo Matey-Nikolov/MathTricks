804ebd30ce09d9416acd7295424ff69ab85b877c
Matey <matey.nikolov@mail.bg>
2023-06-03 09:15:57 +0300

    Add project files.

---
+++ MathTricks/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace MathTricks
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
